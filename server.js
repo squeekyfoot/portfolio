c851f81977343f2dbed0669c375ff0f25a937249
David Broxton <broxtondude@gmail.com>
2017-07-19 10:24:48 -0700

    Moved files to public folder/Added package.json and server.js

---
+++ server.js
@@ -0,0 +1,12 @@
+'use strict';
+
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser').urlencoded({extended: true});
+const PORT = process.env.PORT || 3000;
+
+app.use(express.static('./public'));
+
+app.listen(PORT, function() {
+  console.log(`Server has started. Listening to port ${PORT}`);
+})
