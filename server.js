ff502d6f9ae8c93c5b7a06468af63443a2094a3c
David Broxton <broxtondude@gmail.com>
2017-07-19 11:33:09 -0700

    Removed unnecessry body-parser dependancy

--- server.js
+++ server.js
@@ -2,7 +2,7 @@
 
 const express = require('express');
 const app = express();
-const bodyParser = require('body-parser').urlencoded({extended: true});
+
 const PORT = process.env.PORT || 3000;
 
 app.use(express.static('./public'));
